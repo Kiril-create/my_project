71efe3c81018745a2e9a4f2249bb73a3e12d8ca3
Kiril-create <shabalov192837465@mail.ru>
2026-03-17 21:32:42 +0300

    Update steam_parser.py

--- steam_parser.py
+++ steam_parser.py
@@ -1,26 +1,39 @@
-from random import uniform
+# from random import uniform
 from time import sleep
 
 from bs4 import BeautifulSoup
-from fake_headers import Headers
-import requests
+from selenium.webdriver.common.by import By
+# from fake_headers import Headers
+# import requests
+
+from parsers.utils_str import get_driver, price_to_float
+
+
+# def get_html(url):
+#     counter = 0
+#     while True:
+#         headers = Headers(headers=True).generate()
+#         print('Отправляю запрос...', end=' ')
+#         response = requests.get(url, headers=headers)
+#         if response.status_code == 200:
+#             print('ok!')
+#             write_html(response.text)
+#             return response.text
+#         sleep(uniform(2, 5))
+#         print(response.status_code)
+#         counter += 1
+#         if counter >= 50:
+#             print('Превышен лимит запросов!')
+#             return False
 
 
 def get_html(url):
-    counter = 0
-    while True:
-        headers = Headers(headers=True).generate()
-        print('Отправляю запрос...')
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            print('ok!')
-            write_html(response.text)
-            return response.text
-        sleep(uniform(0.5, 1.5))
-        counter += 1
-        if counter >= 30:
-            print('Превышен лимит запросов!')
-            return False
+    driver = get_driver()
+    print('запускаю chrome...')
+    driver.get(url)
+    print('открыл страницу, ждем ...')
+    sleep(10)
+    return driver.page_source
 
 
 def get_wishlist(url):
@@ -30,40 +43,34 @@ def get_wishlist(url):
         panels = soup.find_all('div', class_='Panel')
         # первые 6 панелей нам не нужны
         all_games = []
-        counter = 0
         for panel in panels[8::2]:
-            name_url = panel.find('div', class_='pMrnNJp5sDA-').find('a')
-            name = name_url.text
-            url = name_url['href']
+            title_url = panel.find('div', class_='-irQN7ogs-M-').find('a')
+            title = title_url.text
+            url = title_url['href']
             divs = panel.find_all('div')
-            price = -1
+            original_price = -1
             actual_price = -1
             discount = 0
             for div in divs:
                 text = div.text.strip()
                 if text.endswith(' руб'):
-                    price = text
-                    if '%' not in price:
-                        price = int(price.split()[0])
-                        actual_price = price
+                    original_price = text
+                    if '%' not in original_price:
+                        original_price = price_to_float(original_price.split()[0])
+                        actual_price = original_price
                     else:
-                        parts = price.split('%')
-                        discount = parts[0] + '%'
-                        # -30%880 руб616 руб
-                        price, actual_price = parts[1].split('руб')[:2]
-                        price = int(price.strip())
-                        actual_price = int(actual_price.strip())
-            print(name, price, actual_price, discount)
+                        parts = original_price.split('%')
+                        discount = float(parts[0].lstrip('-')) / 100
+                        actual_price = parts[1].split('руб')[1]
+                        actual_price = price_to_float(actual_price)
+                        original_price = round(actual_price / discount, 2)
             game_info = {}
-            game_info['name'] = name
+            game_info['title'] = title
             game_info['url'] = url
-            game_info['price'] = price
+            game_info['original_price'] = original_price
             game_info['actual_price'] = actual_price
             game_info['discount'] = discount
             all_games.append(game_info)
-            counter += 1
-            if counter >= 5:
-                break
         return all_games
     return False
 
@@ -74,4 +81,5 @@ def write_html(html):
 
 
 if __name__ == '__main__':
-    get_wishlist('https://store.steampowered.com/wishlist/id/ssylo4ka_v_opisanii/')
+    get_wishlist('https://steamcommunity.com/profiles/76561198125406999/wishlist/')
+
