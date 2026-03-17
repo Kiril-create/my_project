ada6cd2702be450f3f9c4f63c827c516754ab6b6
Kiril-create <shabalov192837465@mail.ru>
2026-03-17 21:34:44 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,28 +1,26 @@
 from datetime import datetime
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from db import Base, engine
 
 
-# nullable - поле может быть пустым
-# unique - поле должно быть уникальным
-# index - ускорение поиска по полю, но трата памяти
-
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     full_name = Column(String, nullable=False)
-    wishlist = Column(String, unique=True, nullable=False)
-    telegram_id = Column(Integer, unique=True)
+    wishlist = Column(String, nullable=False)  # вернуть unique=True
+
+    telegram_id = Column(Integer)  # вернуть unique=True
     region = Column(String, nullable=False, index=True)
     registration_date = Column(DateTime, default=datetime.now)
+    age = Column(Integer, nullable=True)
 
     games = relationship(
         'Game',
         secondary='user_game',
         back_populates='users',
-        lazy='joined'
+        lazy='joined',
     )
 
     def __str__(self):
@@ -32,22 +30,25 @@ class User(Base):
 class Game(Base):
     __tablename__ = 'games'
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    title = Column(String, nullable=False)
     url = Column(String, unique=True, nullable=False)
-    price = Column(Integer, nullable=False)
-    actual_price = Column(Integer, nullable=False)
-    discount = Column(Integer, nullable=True)
+    original_price = Column(Float, nullable=False)
+    actual_price = Column(Float, nullable=False)
+    discount = Column(Float, nullable=True)
+    igm_actual_price = Column(Float, nullable=True)
+    igm_url = Column(String, unique=True, nullable=True)
+    steambuy_actual_price = Column(Float, nullable=True)
+    steambuy_url = Column(String, unique=True, nullable=True)
 
     users = relationship(
         'User',
         secondary='user_game',
         back_populates='games',
-        lazy='joined'
+        lazy='joined',
     )
 
     def __repr__(self):
         return f'игра {self.id} - {self.name} - {self.url} - {self.price} - {self.actual_price} - {self.discount}'
-
 
 
 class UserGame(Base):
@@ -64,11 +65,10 @@ class UserGame(Base):
         index=True,
     )
 
-# id game_id user_id
-# 1     7      12
-# 2     5      12
-# 3     2      6
-# 4     7      11
+
+if __name__ == '__main__':
+    Base.metadata.create_all(bind=engine)
+
 
 
 if __name__ == '__main__':
